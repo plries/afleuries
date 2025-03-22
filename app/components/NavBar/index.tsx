5d10ce662098c41b954d11de8f3a8c9a3b4e3831
plries <apaulries@gmail.com>
2025-03-22 14:50:17 -0700

    adjust import order

--- app/components/NavBar/index.tsx
+++ app/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { motion } from "framer-motion";
 import Link from "next/link";
+import { motion } from "framer-motion";
 import { MenuButton } from "./MenuButton";
 import { NavButtons } from "./NavButtons";
 import { useNavBar } from "./useNavBar";
