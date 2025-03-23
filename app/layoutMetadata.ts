58a0fdda4709d02ef5540db498b2cfb4375d08f5
plries <apaulries@gmail.com>
2025-03-23 02:42:51 -0700

    moved metadata back to separate file

---
+++ app/layoutMetadata.ts
@@ -0,0 +1,76 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    metadataBase: new URL("https://afleuries.com"),
+    alternates: {
+      canonical: "/",
+    },
+    title: "Afleuries Illustrated: Live Portrait Art",
+    publisher: "Abigail Aries",
+    description:
+      "Live digital portraits, drawn on the spot and projected in real time for everyone to see. It’s live art in action—plus a keepsake to take home!",
+    authors: { name: "Abigail Aries" },
+    keywords: [
+      "abigail aries",
+      "abby aries",
+      "afleuries",
+      "afleuries illustrated",
+      "live portraits",
+      "live portrait art",
+      "live event artist",
+      "digital portraits",
+      "custom illustrations",
+      "live sketching",
+      "event entertainment",
+      "wedding artist",
+      "personalized art",
+      "guest portraits",
+      "interactive art",
+      "digital art",
+      "live drawing",
+      "event keepsakes",
+      "wedding keepsakes",
+      "custom event souvenirs",
+      "illustration artist",
+      "live art projection",
+      "party entertainment",
+      "corporate event artist",
+      "creative event services",
+      "digital illustration",
+      "artistic experiences",
+      "unique event ideas",
+      "event personalization",
+      "memorable event gifts",
+      "event photography alternative",
+      "live painting",
+      "modern event trends",
+      "creative wedding ideas",
+      "digital caricatures",
+      "portrait artist for events",
+    ],
+    viewport: "width=device-width, initial-scale=1.0",
+    openGraph: {
+      title: "Afleuries Illustrated: Live Portrait Art",
+      description:
+        "Live digital portraits, drawn on the spot and projected in real time for everyone to see. It’s live art in action—plus a keepsake to take home!",
+      images: [
+        {
+          url: "https://afleuries.com/opengraph-image.png",
+          width: 1200,
+          height: 630,
+          alt: "Afleuries Illustrated: Live Portrait Art",
+        },
+      ],
+      siteName: "Afleuries Illustrated: Live Portrait Art",
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+    icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon-32x32.png",
+      apple: "/apple-touch-icon.png",
+    },
+  };
+  
